713f3a554c6b070cba12962880c8d0df88256036
eagle <nubmortis@gmail.com>
2024-08-15 13:58:26 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var progress = document.getElementById("progress");
+var song = document.getElementById("song");
+var ctrlIcon = document.getElementById("ctrlIcon");
+
+song.onloadedmetadata = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+function playPause(){
+    if(ctrlIcon.classList.contains("fa-pause")){
+        song.pause();
+        ctrlIcon.classList.remove("fa-pause");
+        ctrlIcon.classList.add("fa-play");
+    } else{
+        song.play();
+        ctrlIcon.classList.remove("fa-play");
+        ctrlIcon.classList.add("fa-pause");
+    }
+}
+
+if(song.play()){
+    setInterval(() => {
+        progress.value = song.currentTime;
+    }, 500)
+}
+
+progress.onchange = function(){
+    song.play();
+    song.currentTime = progress.value;
+    ctrlIcon.classList.remove("fa-play");
+    ctrlIcon.classList.add("fa-pause");
+}
